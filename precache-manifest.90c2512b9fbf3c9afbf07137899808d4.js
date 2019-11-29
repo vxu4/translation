25c718e9a9d785d1bee903abec63262036c2e691
vxu4 <vixixu68@gmail.com>
2019-11-29 17:17:29 -0500

    Updates

---
+++ precache-manifest.90c2512b9fbf3c9afbf07137899808d4.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8a42778e7ada48187daa814af1facd3",
+    "url": "/translation/index.html"
+  },
+  {
+    "revision": "2c34dcb67482e227b8e2",
+    "url": "/translation/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "7f7dc84297db2ee7d6ad",
+    "url": "/translation/static/js/2.d7e1da54.chunk.js"
+  },
+  {
+    "revision": "2c34dcb67482e227b8e2",
+    "url": "/translation/static/js/main.84f1a061.chunk.js"
+  },
+  {
+    "revision": "13ec8553b2a41aaef15a",
+    "url": "/translation/static/js/runtime-main.0361cdce.js"
+  },
+  {
+    "revision": "e562352a20ec33722284fc2299619748",
+    "url": "/translation/static/media/1_1_frame2.e562352a.svg"
+  },
+  {
+    "revision": "f06b33404ceb62cc8b2cd233cd90dde9",
+    "url": "/translation/static/media/1_2_frame2.f06b3340.svg"
+  },
+  {
+    "revision": "ac0c6295e03ec9aee331935ef82e03e4",
+    "url": "/translation/static/media/1_3_frame2.ac0c6295.svg"
+  },
+  {
+    "revision": "161790e3db95b07067e92baf4dc69de8",
+    "url": "/translation/static/media/1_4_frame2.161790e3.svg"
+  },
+  {
+    "revision": "c0902f2789b58344765e6cfe5fa7a9ae",
+    "url": "/translation/static/media/2_1_frame2.c0902f27.svg"
+  },
+  {
+    "revision": "f826b1b20e3ddb53b99ab148b04d5e76",
+    "url": "/translation/static/media/2_2_frame2.f826b1b2.svg"
+  },
+  {
+    "revision": "510254829a751b69ac23214bb29dffba",
+    "url": "/translation/static/media/2_3_frame2.51025482.svg"
+  },
+  {
+    "revision": "a4ec0caa67790c8d607c6326322c7f64",
+    "url": "/translation/static/media/2_4_frame2.a4ec0caa.svg"
+  },
+  {
+    "revision": "3a80d8bf735659af98483721d580df24",
+    "url": "/translation/static/media/HongCunTri.3a80d8bf.jpg"
+  },
+  {
+    "revision": "856fd87ef9e021ccc9ff8e61245b7896",
+    "url": "/translation/static/media/LaceFull.856fd87e.svg"
+  },
+  {
+    "revision": "f9ec9ba9ef9662c9252f854ba971bc6c",
+    "url": "/translation/static/media/burlapCover_1.f9ec9ba9.svg"
+  },
+  {
+    "revision": "83db2d96a0a895b34ddb5a05a2604055",
+    "url": "/translation/static/media/burlapFamily.83db2d96.png"
+  },
+  {
+    "revision": "901493ae24270264464895d645988a84",
+    "url": "/translation/static/media/conversationsCover_2.901493ae.svg"
+  },
+  {
+    "revision": "3b6258c05ba043b553955e96b16ae987",
+    "url": "/translation/static/media/dataCover_1.3b6258c0.svg"
+  },
+  {
+    "revision": "616f9d3af77ce0fa824ee90268a4330b",
+    "url": "/translation/static/media/embodimentCover_1.616f9d3a.svg"
+  },
+  {
+    "revision": "cbef36a4fefb815d4aace52bf27fe1f8",
+    "url": "/translation/static/media/embodimentsClose1.cbef36a4.jpg"
+  },
+  {
+    "revision": "277f1dde58e1bc0b853dac4cf8d8f82b",
+    "url": "/translation/static/media/fuCover_1.277f1dde.svg"
+  },
+  {
+    "revision": "f1713d8ee64e26874aa8be4ad0bdbd59",
+    "url": "/translation/static/media/fuDao.f1713d8e.png"
+  },
+  {
+    "revision": "78899d48d7f2e38cf461f2f98d269f47",
+    "url": "/translation/static/media/grass.78899d48.svg"
+  },
+  {
+    "revision": "f245bf083078ec467a7ee2ba986811a3",
+    "url": "/translation/static/media/hongCunClose.f245bf08.png"
+  },
+  {
+    "revision": "31ae471db4eec5992c6057cec32291a4",
+    "url": "/translation/static/media/hongCunLandscape.31ae471d.png"
+  },
+  {
+    "revision": "ccca9dd60fdcb4a9e87077ec248efd94",
+    "url": "/translation/static/media/hongcunCover_1.ccca9dd6.svg"
+  },
+  {
+    "revision": "e6d5b2dbf02f4ebf5bc0c7d6f30347ff",
+    "url": "/translation/static/media/huskLamp0.e6d5b2db.jpg"
+  },
+  {
+    "revision": "16e8966f0a69d7862f5ccfe4a02fb56f",
+    "url": "/translation/static/media/huskLamp1.16e8966f.jpg"
+  },
+  {
+    "revision": "70cccd726fb6f2260227ab7ac463c42a",
+    "url": "/translation/static/media/huskLamp2.70cccd72.jpg"
+  },
+  {
+    "revision": "c08ddd39d1f04e54401fe84368a2650a",
+    "url": "/translation/static/media/huskLamp3.c08ddd39.jpg"
+  },
+  {
+    "revision": "8a959da9813c0b45bf22205b34748c96",
+    "url": "/translation/static/media/huskLamp4.8a959da9.jpg"
+  },
+  {
+    "revision": "f1d8a2b8b827e9905f78964936aa48c0",
+    "url": "/translation/static/media/huskLamp5.f1d8a2b8.jpg"
+  },
+  {
+    "revision": "0f1880498da70de4b29a1a2b64b4a443",
+    "url": "/translation/static/media/lawnCover_2.0f188049.svg"
+  },
+  {
+    "revision": "6ec0831df2016b83dcb5a4929e61965f",
+    "url": "/translation/static/media/lawnmowing.6ec0831d.svg"
+  },
+  {
+    "revision": "75eee63ae98f738bc8791c9804e8e401",
+    "url": "/translation/static/media/map1.75eee63a.png"
+  },
+  {
+    "revision": "dc064f20343537289b9ab0ad536fc4c3",
+    "url": "/translation/static/media/quilt8.dc064f20.svg"
+  },
+  {
+    "revision": "d808e9ccde7c18e9120ded1aa41ee755",
+    "url": "/translation/static/media/quiltCover_1.d808e9cc.svg"
+  },
+  {
+    "revision": "48b876889bd0bcdc3d3699cd4df8d989",
+    "url": "/translation/static/media/quiltWIP.48b87688.svg"
+  },
+  {
+    "revision": "6e94ff06c5c6508007a3169f172a2d1a",
+    "url": "/translation/static/media/touchingNoses.6e94ff06.png"
+  }
+]);
